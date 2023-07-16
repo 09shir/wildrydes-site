f8cc705bb4a819d08529994c4251c687ab998099
Roy Shi <royshi@Roys-MacBook-Pro-2.local>
2023-07-15 19:20:30 -0700

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_pXIx3KX9H', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5o9paq6dpv3pkli6udlgsf1ohn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
